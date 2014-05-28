6540b07d63f1f38c9ca79e2d08468840b26f2b1f
Vittorio Strada <vittorio@falcon.local>
2014-05-28 16:58:07 +0200

    aggiunto elemento
    lista prodotti

--- Elencoprodotti.rb
+++ Elencoprodotti.rb
@@ -1,10 +1,15 @@
-def leggi_dati
-	puts "sto leggendo i dati"
+def leggi_dati(lst)
+  lst << ["computer", 400]
+  lst << ["tastiera", 15]
+  lst << ["mouse", 8]
 end
 
-def stampa_dati
-	puts "sto stampando i dati"
+def stampa_dati(lst)
+  lst.each do |prod, prezzo|
+     printf("prodotto: %20s     prezzo: %10.2f\n", prod, prezzo)
+  end
 end
 
-leggi_dati
-stampa_dati
+lista = []
+leggi_dati(lista)
+stampa_dati(lista)
